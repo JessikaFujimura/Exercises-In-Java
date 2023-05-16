ee390caf166203e1132d77ea71ca49251a4d6df2
Jéssika <jessikafujimura@gmail.com>
2023-05-15 22:38:35 -0300

    Resolution exercise area of circle

---
+++ src/Easy/AreaOfCircle.java
@@ -0,0 +1,19 @@
+package Easy;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AreaOfCircle {
+
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("en", "US"));
+        double pi = Double.parseDouble(String.format("%.5f", Math.PI));
+
+        Scanner sc = new Scanner(System.in);
+        double radius = sc.nextDouble();
+        double area = (Math.pow(radius, 2) * pi);
+        System.out.printf("A=%.4f", area);
+        sc.close();
+    }
+
+}
